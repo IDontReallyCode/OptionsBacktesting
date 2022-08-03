2d7d104eae5afbf88d2e8c0c620fc8fd46970e31
IDontReallyCode <92130644+IDontReallyCode@users.noreply.github.com>
2022-08-03 14:44:57 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my attempt to build a backtesting framework that deals with options
---
+++ dataobjects/oneticker.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import datetime
+
+class onticker():
+    """
+        oneticker will contain the time series data of a ticker and it's option chain over a specific frequency (daily or intraday)
+    """
+    def __init__(self, tickername:str, tickertimeseries:pd.DataFrame, optionchaintimeseries:pd.DataFrame) -> None:
+        """
+            We load the entire data sample at once.
+            tickertimeseries would be a pandas.Dataframe timestamp, OHLC with a integer as the index.
+        """
+        self.ticker = tickername
+        self.tickerts = tickertimeseries
+        self.optionts = optionchaintimeseries
+        self.currenttime = 0
+
+    
+    def getnewbar(self) -> dict:
+        """
+            returns the next candle bar
+        """
+        self.currenttime+=1
+
+
+    def resettimer(self) -> None:
+        self.currenttime=0
