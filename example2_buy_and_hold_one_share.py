adf6e5c4fd2b8a44832596316a361732130258ff
IDontReallyCode <92130644+IDontReallyCode@users.noreply.github.com>
2024-09-13 13:45:05 -0500

    Update to example2, added some comments

--- example2_buy_and_hold_one_share.py
+++ example2_buy_and_hold_one_share.py
@@ -10,6 +10,7 @@ import pandas as pd
 import optionbacktesting as obt
 # import datetime
 import matplotlib.pyplot as plt
+import matplotlib.axes as axes
 
 from optionbacktesting.broker import ASSET_TYPE_STOCK, BUY_TO_OPEN
 
@@ -59,18 +60,31 @@ def main():
     """
         Initialize the objects we need to run the strategy back tester
     """
+    # First, we need to create an Account object to hold the positions and the cash
+    # We will initialize the account with 1000 USD
     myaccount = [obt.Account(deposit=1000)]
+    # We need to create a Market object to hold the data
+    # We will create a OneTicker object to hold the stock data for one ticker. We can create multiple OneTicker objects if we have multiple tickers
     tickerRANDOM = obt.OneTicker(tickername='RANDOM', tickertimeseries=stockdata, optionchaintimeseries=pd.DataFrame())
+    # We will create a Market object to hold the OneTicker objects
     mymarket = obt.Market([tickerRANDOM],['RANDOM'])
+    # We need to create a Dealer object to handle the orders
+    # We will initialize the dealer with the marketdata, but there are other parameters that we could set.
     mydealer = obt.Dealer(marketdata=mymarket)
+    # We need to create a Strategy list to hold the list of all strategies. (Only one strategy here)
     mystrategy = [MyStrategy()]                         # Needs to be a list
+    # Finally, we need to create a Chronos object to run the back test
     mychronos = obt.Chronos(marketdata=mymarket, marketdealer=mydealer, clientaccounts=myaccount, clientstrategies=mystrategy, chronology=uniquedaydates)
 
     # Tell Chronos were you want to start testing the strategy in the time series of unique time steps by sending the index
     mychronos.primingthestrategyat(0)
 
+    # Run the back test
     mychronos.execute()
 
+    # Plot the results
+    ax1: axes._axes.Axes
+    ax2: axes._axes.Axes
     print(myaccount[0].positions.mypositions)
     fig, (ax1, ax2) = plt.subplots(2,1, sharex=True, sharey=False)
     ax1.plot(uniquedaydates, myaccount[0].totalvaluests)
